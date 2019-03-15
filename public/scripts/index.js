1e17ee60273a15a73d3c34216372c497ecbc579e
Nathan <nathan@kcenglishacademy.com>
2019-03-15 13:49:05 +0100

    Clean up code

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,15 @@
 
 $(document).ready(function() {
     var option;
+    // Set empty array
     var usersList = [];
+    // hide initial state of divs
      $("#javascript").hide();
      $("#php").hide();
      $("#java").hide();
      $("#Cplusplus").hide();
+
+     // Function for submit
     $("#submit").click(function(e){
         e.preventDefault();
         var firstName = $("input[name=firstname]").val();
@@ -14,34 +18,27 @@ $(document).ready(function() {
 
         // Add User info to array
         usersList.push({name: firstName, lastname: lastName, course: courseJS});
-        
-        // empty node
-        //$("#javascript").empty(); FIND THE METHOD TO EMPTY THE ARRAY
 
-        console.log(courseJS);
         // sort array
         for (i = 0; i < usersList.length;i++) {
             if (usersList[i].course == "JavaScript") {
                 $("#javascript").append("<div class=\"col-xs-6\">" + usersList[i].name + "</div>" + "<div class=\"col-xs-6\">" + usersList[i].lastname + "</div>");
                 $("#javascript").show();
-            //$("#last").append("<p>" + usersList[i].lastname + "</p>");
-            //$("#course").append("<h1>" + usersList[i].course + "</h1>");
             } else if (usersList[i].course == "PHP") {
                 $("#php").append("<div class=\"col-xs-6\">" + usersList[i].name + "</div>" + "<div class=\"col-xs-6\">" + usersList[i].lastname + "</div>");
                 $("#php").show();
-            } else if (usersList[i].course == "JAVA ") {
+            } else if (usersList[i].course == "JAVA") {
                 $("#java").append("<div class=\"col-xs-6\">" + usersList[i].name + "</div>" + "<div class=\"col-xs-6\">" + usersList[i].lastname + "</div>");
                 $("#java").show();
             } else if (usersList[i].course == "C++") {
                 $("#Cplusplus").append("<div class=\"col-xs-6\">" + usersList[i].name + "</div>" + "<div class=\"col-xs-6\">" + usersList[i].lastname + "</div>");
                 $("#Cplusplus").show();
             }
-            
+            // Empty node array
             usersList.length = 0;
         }
-        $("#course").text();
-        $("#first").text();
-        $("#last").text();
+
+        // Reset form after submission
         $("#form")[0].reset();
             
     });
@@ -49,8 +46,6 @@ $(document).ready(function() {
     $(".selector").click(function(e) {
         e.preventDefault();
         option = $(this).html();
-        console.log($(this).html());
-        console.log(e);
     });
 });
 
